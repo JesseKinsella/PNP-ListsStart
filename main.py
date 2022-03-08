097ea7ae61ecd9eaaf58eed2f32cd2734ee03702
Jesse Kinsella <89218189+JesseKinsella@users.noreply.github.com>
2022-03-08 18:18:44 +0000

    added code

---
+++ main.py
@@ -0,0 +1,16 @@
+#lists -> an ordered list of information
+
+list_ints = [1,24,78]
+
+fav_movies = ['Sandlot', 'The Lego Movie', 'Dune']
+
+print(fav_movies)
+
+#grab items from list
+print(fav_movies [1]) #why second item? lists use 0 based lists, Sandlot = 0
+
+print(fav_movies [0])
+
+fav_num = [3,16,84]
+
+print(fav_num[0])
